de01d691b4a7b9fe00bb57ca5f4f22796c18835c
amm1394 <59041940+amm1394@users.noreply.github.com>
2025-11-27 17:53:12 +0330

    Add Concentration column to Measurements table
    Introduces a nullable 'Concentration' column to the 'Measurements' table via a new migration. Also updates the model snapshot and corrects a configuration key in appsettings.json from 'ExpiryInMinutes' to 'ExpiryMinutes'.

---
+++ Src/Infrastructure/Migrations/20251127141215_Add_Users2.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_Users2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "Concentration",
+                table: "Measurements",
+                type: "float",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Concentration",
+                table: "Measurements");
+        }
+    }
+}
