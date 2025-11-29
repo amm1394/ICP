31d53bd64b118c24d837c2a50601f8ae01134a6c
amm1394 <59041940+amm1394@users.noreply.github.com>
2025-11-29 18:55:06 +0330

    Update test project dependencies and solution file
    Upgraded NuGet package versions in Tests.csproj to .NET 10.0 compatible releases and added explicit Azure.Core reference to resolve runtime issues. Also added Tests.csproj to the solution file and updated IntegrationTests.cs with missing using directives.

--- New_Src/Tests/IntegrationTests.cs
+++ New_Src/Tests/IntegrationTests.cs
@@ -1,7 +1,10 @@
+using System;
 using System.Net;
+using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Net.Http.Json;
 using System.Text;
+using System.Threading.Tasks;
 using FluentAssertions;
 using Xunit;
 
